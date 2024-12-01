5c5b20cd99a32d8b92da1e5821aa8a78ba078fa0
f0xgirl <Brobbyjones@gmail.com>
2024-11-30 21:06:46 -0600

    added controller support
    planning on adding enemies and stuff

--- assets/script/resources scripts/level_data.gd
+++ assets/script/resources scripts/level_data.gd
@@ -1,11 +1,14 @@
 extends Resource
 class_name level_data
 
+@export var score: int = 0
 @export_group("camera boundaries")
 @export var left: int
 @export var top: int
 @export var right: int
 @export var bottom: int
+
+
 
 
 func get_limit(index: int):
